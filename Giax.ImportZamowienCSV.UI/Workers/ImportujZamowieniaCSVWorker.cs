389d3181b3e5931f4838c6347626ca20ab8f04fe
it01 <it01@GIAXIT09>
2024-09-16 11:12:14 +0200

    Dodanie możliwości importu w wersji ENG + poprawa UI importu

--- Giax.ImportZamowienCSV.UI/Workers/ImportujZamowieniaCSVWorker.cs
+++ Giax.ImportZamowienCSV.UI/Workers/ImportujZamowieniaCSVWorker.cs
@@ -34,6 +34,8 @@ using Soneta.Core;
 using ServiceStack;
 using CsvHelper.Configuration.Attributes;
 using Soneta.Tools;
+using Soneta.Data.QueryDefinition;
+using System.Threading;
 
 [assembly: Worker(typeof(ImportujZamowieniaCSVWorker), typeof(DokHandlowe))]
 
@@ -52,7 +54,7 @@ namespace Giax.ImportZamowienCSV.UI.Workers
 
         [Action("Giax/Importuj zamowienia Amazon CSV", Icon = ActionIcon.ArrowUp, Mode = ActionMode.SingleSession | ActionMode.ConfirmSave | ActionMode.Progress)]
       
-        public MessageBoxInformation CSV()
+        public object CSV()
         {
             int added_positions_count = 0;
             int added_orders_count = 0;
@@ -64,11 +66,6 @@ namespace Giax.ImportZamowienCSV.UI.Workers
            
            
             string filepath = @params.FilePath;
-
-          //  if (!File.Exists(filepath))
-            //{
-               // return new MessageBoxInformation("Błąd", "Plik nie istnieje.");
-//            }
 
             List<Pozycja> pozycje;
 
@@ -83,97 +80,98 @@ namespace Giax.ImportZamowienCSV.UI.Workers
 
             List<string> numery_zamowien = GetUniqueOrderNumbers(pozycje);
 
-            List<DokumentHandlowy> zamowienia = new List<DokumentHandlowy>(); 
+            List<DokumentHandlowy> zamowienia = new List<DokumentHandlowy>();
 
             using (var t = Session.Logout(true))
             {
-                foreach (var numer in numery_zamowien)
+                TraceInfo.HideProgressWindow();
+                TraceInfo.ShowProgressWindow();
+                for (int i = 0; i < numery_zamowien.Count; i++)
                 {
-                    List<Pozycja> filtrowane_pozycje = pozycje.Where(p => p.NumerZamowieniaPO == numer && !p.Dostepnosc.Contains("Anulowano")).ToList();
-
-                    if (!filtrowane_pozycje.Any())
-                        continue;
-
-                    added_positions_count += filtrowane_pozycje.Count;
-                    added_orders_count++;
-
-                    DokumentHandlowy dokument = new DokumentHandlowy();
-                    HandelModule.GetInstance(Session).DokHandlowe.AddRow(dokument);
-
-                    dokument.Definicja = HandelModule.GetInstance(Session).DefDokHandlowych.WgSymbolu[@params.NazwaDefDok];
-                    dokument.Obcy.Numer = numer;
-                    dokument.Magazyn = HandelModule.GetInstance(Session).Magazyny.Magazyny.WgNazwa[@params.NazwaMag];
-
-                    var pierwszaPozycja = filtrowane_pozycje.First();
-                    var lokzalizacja = pierwszaPozycja.Lokalizacja.Substring(0, 4);
-                    //var lokzalizacja = "XS";
-                    var crmmodule = CRMModule.GetInstance(Session);
-                    var lokalziacje_kont = crmmodule.Lokalizacje.CreateView().ToList();
-
-                    foreach (Lokalizacja lok in lokalziacje_kont)
-                    {
-                        if (lok.Nazwa.Contains(lokzalizacja))
-                        {
-                            dokument.Kontrahent = (Kontrahent)lok.Kontrahent;
-                            dokument.OdbiorcaMiejsceDostawy = lok;
-                            czy_kontrahent = true;
-                            break;
-                        }
-                    }
-
-                    if (!czy_kontrahent)
-                        return new MessageBoxInformation("Błąd", $"Nie znaleziono kontrahenta dla lokalizacji: {lokzalizacja}");
-
-                    dokument.Data = Date.Parse(filtrowane_pozycje.First().DataZamowienia);
-                    dokument.DataOtrzymania = Date.Parse(filtrowane_pozycje.First().DataOtrzymania);
-
                     
-
-                    if (@params.CzyZaakceptowany) dokument.Potwierdzenie = PotwierdzenieDokumentuHandlowego.Zaakceptowany;
-                    if (@params.CzyZatwierdzony) dokument.Potwierdzenie = PotwierdzenieDokumentuHandlowego.Potwierdzony;
-
-                    var towar_modue = TowaryModule.GetInstance(Session).Towary;
-                    foreach (var poz in filtrowane_pozycje)
+                    foreach (var numer in numery_zamowien)
                     {
-                        var pozycjaDokHandlowego = Session.AddRow(new PozycjaDokHandlowego(dokument));
+                        TraceInfo.WriteProgress("Obiekt: " + i + " (pozostało: " + numery_zamowien.Count + ")");
+                    TraceInfo.SetProgressBar(new Percent(i + 1, numery_zamowien.Count));
+                        List<Pozycja> filtrowane_pozycje = pozycje.Where(p => p.NumerZamowieniaPO == numer && !p.Dostepnosc.Contains("Anulowano")).ToList();
+
+                        if (!filtrowane_pozycje.Any())
+                            continue;
+
+                        added_positions_count += filtrowane_pozycje.Count;
+                        added_orders_count++;
+
+                        DokumentHandlowy dokument = new DokumentHandlowy();
+                        HandelModule.GetInstance(Session).DokHandlowe.AddRow(dokument);
+
+                        dokument.Definicja = HandelModule.GetInstance(Session).DefDokHandlowych.WgSymbolu[@params.NazwaDefDok];
+                        dokument.Obcy.Numer = numer;
+                        dokument.Magazyn = HandelModule.GetInstance(Session).Magazyny.Magazyny.WgNazwa[@params.NazwaMag];
+
+                        var pierwszaPozycja = filtrowane_pozycje.First();
+                        var lokzalizacja = pierwszaPozycja.Lokalizacja.Substring(0, 4);
+                       // var lokzalizacja = "XS";
+                        var crmmodule = CRMModule.GetInstance(Session);
+                        var lokalziacje_kont = crmmodule.Lokalizacje.CreateView().ToList();
+
+                        foreach (Lokalizacja lok in lokalziacje_kont)
+                        {
+                            if (lok.Nazwa.Contains(lokzalizacja))
+                            {
+                                dokument.Kontrahent = (Kontrahent)lok.Kontrahent;
+                                dokument.OdbiorcaMiejsceDostawy = lok;
+                                czy_kontrahent = true;
+                                break;
+                            }
+                        }
+
+                        if (!czy_kontrahent)
+                            return new MessageBoxInformation("Błąd", $"Nie znaleziono kontrahenta dla lokalizacji: {lokzalizacja}");
+
+                        dokument.Data = Date.Parse(filtrowane_pozycje.First().DataZamowienia);
+                        dokument.DataOtrzymania = Date.Parse(filtrowane_pozycje.First().DataOtrzymania);
+
+
+
+                        if (@params.CzyZaakceptowany) dokument.Potwierdzenie = PotwierdzenieDokumentuHandlowego.Zaakceptowany;
+                        if (@params.CzyZatwierdzony) dokument.Potwierdzenie = PotwierdzenieDokumentuHandlowego.Potwierdzony;
+
+                        var towar_modue = TowaryModule.GetInstance(Session).Towary;
+                        foreach (var poz in filtrowane_pozycje)
+                        {
+                            var pozycjaDokHandlowego = Session.AddRow(new PozycjaDokHandlowego(dokument));
+
+                            Towar towar;
+                            try
+                            {
+                               towar = towar_modue.WgEAN[poz.EAN].First();
+
+                            }
+                            catch (Exception ex)
+                            {
+                                return new MessageBoxInformation("Błąd", $"Nie znaleziono towaru dla EAN: {poz.EAN}");
+                            }
+
+                            //towar = TowaryModule.GetInstance(Session).Towary.WgEAN["5901035500211"].First();
+                            pozycjaDokHandlowego.Towar = towar;
+                            pozycjaDokHandlowego.Ilosc = new Quantity(poz.Ilosc, pozycjaDokHandlowego.Towar.Jednostka.Kod);
+                            pozycjaDokHandlowego.Cena = new DoubleCy(poz.KosztJednostkowy);
+                        }
+
+                        if (!@params.CzyBufor) dokument.Stan = StanDokumentuHandlowego.Zatwierdzony;
+                        dokument.Features["Dane kuriera"] = "UPS";
+                        dokument.Features["Data_Dostawy_Zam"] = Date.Parse(filtrowane_pozycje.First().DataZamowienia).AddDays(2);
+                        zamowienia.Add(dokument);
+
                        
-                        Towar towar;
-                        try
-                        {
-                            towar = towar_modue.WgEAN[poz.EAN].First();
-
-                        }
-                        catch (Exception ex)
-                        {
-                            return new MessageBoxInformation("Błąd", $"Nie znaleziono towaru dla EAN: {poz.EAN}");
-                        }
-
-                        //towar = TowaryModule.GetInstance(Session).Towary.WgEAN["5901035500211"].First();
-                        pozycjaDokHandlowego.Towar = towar;
-                        pozycjaDokHandlowego.Ilosc = new Quantity(poz.Ilosc, pozycjaDokHandlowego.Towar.Jednostka.Kod);
-                        pozycjaDokHandlowego.Cena = new DoubleCy(poz.KosztJednostkowy);
                     }
 
-                    if (!@params.CzyBufor) dokument.Stan = StanDokumentuHandlowego.Zatwierdzony;
 
-                    zamowienia.Add(dokument);
-                    
-                    Session.Events.Invoke();
                 }
+                t.CommitUI();
             }
+            return new MessageBoxInformation("Sukces", $"Zaimportowane zamówienia {numery_zamowien.Count}");
 
-            return new MessageBoxInformation("Import CSV")
-            {
-                Text = "Pomyślnie zaimportowano: " + added_orders_count + " zamowień i " + added_positions_count + " pozycji.",
-                OKHandler = () =>
-                {
-                    using (var t = @params.Session.Logout(true))
-                    {
-                        t.Commit();
-                    }
-                    return "Operacja została zakończona pomyślnie";
-                }
-            };
         }
 
 
@@ -183,28 +181,43 @@ namespace Giax.ImportZamowienCSV.UI.Workers
             var pozycje = new List<Pozycja>();
             var fs = Session.GetService<IFileSystemService>();
             var data = fs.ReadStream(@params.Plik);
+
             using (var reader = new StreamReader(data))
-            using (var csv = new CsvHelper.CsvReader(reader, CultureInfo.InvariantCulture))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
             {
                 csv.Read();
                 csv.ReadHeader();
+
+                var secondColumnHeader = csv.HeaderRecord[1];
+
+                string poColumn = "PO";
+                string vendorColumn = secondColumnHeader == "Vendor" ? "Vendor" : "Dostawca";
+                string warehouseColumn = secondColumnHeader == "Vendor" ? "Warehouse" : "Wysyłka do lokalizacji";
+                string externalIdColumn = secondColumnHeader == "Vendor" ? "External ID" : "Identyfikator zewnętrzny";
+                string acceptedQuantityColumn = secondColumnHeader == "Vendor" ? "Accepted Quantity" : "Zaakceptowana ilość";
+                string unitCostColumn = secondColumnHeader == "Vendor" ? "Unit Cost" : "Koszt jednostkowy";
+                string totalCostColumn = secondColumnHeader == "Vendor" ? "Total Cost" : "Całkowity koszt";
+                string windowStartColumn = secondColumnHeader == "Vendor" ? "Window Start" : "Data początkowa przedziału czasowego";
+                string windowEndColumn = secondColumnHeader == "Vendor" ? "Window End" : "Data końcowa przedziału czasowego";
+                string availabilityColumn = secondColumnHeader == "Vendor" ? "Availability" : "Dostępność";
+
                 while (csv.Read())
                 {
-                    var iloscString = csv.GetField<string>("Zaakceptowana ilość");
+                    var iloscString = csv.GetField<string>(acceptedQuantityColumn);
                     int ilosc = 0;
                     if (!string.IsNullOrWhiteSpace(iloscString))
                     {
                         int.TryParse(iloscString, NumberStyles.Any, CultureInfo.InvariantCulture, out ilosc);
                     }
 
-                    var kosztJednostkowyString = csv.GetField<string>("Koszt jednostkowy");
+                    var kosztJednostkowyString = csv.GetField<string>(unitCostColumn);
                     double kosztJednostkowy = 0.0;
                     if (!string.IsNullOrWhiteSpace(kosztJednostkowyString))
                     {
                         double.TryParse(kosztJednostkowyString, NumberStyles.Any, CultureInfo.InvariantCulture, out kosztJednostkowy);
                     }
 
-                    var calkowityKosztString = csv.GetField<string>("Całkowity koszt");
+                    var calkowityKosztString = csv.GetField<string>(totalCostColumn);
                     double calkowityKoszt = 0.0;
                     if (!string.IsNullOrWhiteSpace(calkowityKosztString))
                     {
@@ -213,23 +226,20 @@ namespace Giax.ImportZamowienCSV.UI.Workers
 
                     var pozycja = new Pozycja
                     {
-                        EAN = csv.GetField<string>("Identyfikator zewnętrzny"),
+                        EAN = csv.GetField<string>(externalIdColumn),
                         Ilosc = ilosc,
                         KosztJednostkowy = kosztJednostkowy,
-                        NumerZamowieniaPO = csv.GetField<string>("PO"),
-                        DataZamowienia = csv.GetField<string>("Data początkowa przedziału czasowego"),
-                        Dostepnosc = csv.GetField<string>("Dostępność"),
-                        Lokalizacja = csv.GetField<string>("Wysyłka do lokalizacji"),
-                        DataOtrzymania = csv.GetField<string>("Data końcowa przedziału czasowego")
+                        NumerZamowieniaPO = csv.GetField<string>(poColumn),
+                        DataZamowienia = csv.GetField<string>(windowStartColumn),
+                        Dostepnosc = csv.GetField<string>(availabilityColumn),
+                        Lokalizacja = csv.GetField<string>(warehouseColumn),
+                        DataOtrzymania = csv.GetField<string>(windowEndColumn)
                     };
 
                     pozycje.Add(pozycja);
-                   
                 }
             }
 
-
-           
             return pozycje;
         }
 
@@ -272,13 +282,9 @@ namespace Giax.ImportZamowienCSV.UI.Workers
 
         public ImportujZamowieniaCSVWorkerParams(Context context) : base(context)
         {
-          //  AddRequiredVerifierForProperty(nameof(FilePath));
+         
         }
 
-     //   [Context]
-       // [Required]
-       // [Caption("Kolumna - Konto")]
-      //  public NamedStream Plik { get; set; }
 
 
         public Object GetList()
@@ -286,7 +292,7 @@ namespace Giax.ImportZamowienCSV.UI.Workers
             return new FileDialogInfo { Title = "Wybierz pliki z wyciągami", ForbidMultiSelection = false }.AddAllFilesFilter();
         }
 
-        //[Required]
+       
         public string FilePath
         {
             get { return V; }
